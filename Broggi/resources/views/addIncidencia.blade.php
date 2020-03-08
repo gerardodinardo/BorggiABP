ea4c83cd589bbf2b4b24217b9543bd92ef338078
Tritesio <alessiocrusella@gmail.com>
2020-03-08 21:22:11 +0100

    Pantalla Desplegables Hasta Alertante

--- Broggi/resources/views/addIncidencia.blade.php
+++ Broggi/resources/views/addIncidencia.blade.php
@@ -25,30 +25,50 @@
             </div>
         </div>
         <!-- ********** Fin Desplegable 1 ********** -->
+        <script src="{{ asset('/js/addIncidenciaSelecciones.js') }}"></script>{{-- Js resultado aparecer div segun selector --}}
         <div class="desplegable">
             <button class="collapsible"><strong>Identificació - Classificació Alertant</strong></button>
             <div class="content">
-                <select class="custom-select mt-4 mb-4">
+                <select class="custom-select mt-4 mb-4" select id="getFname" onchange="admSelectCheck(this);">
                     <option selected>- Slecciona un tipus d'Alertant-</option>
-                    <option value="1">Centre Sanitari</option>
-                    <option value="2">Persona Afectada</option>
-                    <option value="3">Entorn de l'Afectat</option>
-                    <option value="4">Alertant VIP</option>
+                    <option id="centreSanitari"value="1">Centre Sanitari</option>
+                    <option id="personaAfectada"value="2">Persona Afectada</option>
+                    <option id="entornAfectat" value="3">Entorn de l'Afectat</option>
+                    <option id="alertantVip" value="4">Alertant VIP</option>
                   </select>
+                  {{-- divs a mostrar despues de execute javascript, los divs estan default display:none --}}
+                <div id="divcentreSanitari" style="display:none;">
+                    centreSanitari selected
+                    <div class="form-group row">
+                        <label for="example-metge-input" class="col-sm-2 mt-4 col-form-label">Nom del Metge: </label>
+                        <div class="col-sm-4 mt-4">
+                            <input class="form-control" type="metge" value="" id="example-metge-input" placeholder="">
+                        </div>
+                    </div>
+                </div>  
+                <div id="divpersonaAfectada" style="display:none;">
+                    personaAfectada selected
+                </div>
+                <div id="diventornAfectat" style="display:none;">
+                    <p>entornAfectat selected</p>
+                </div>   
+                <div id="divalertantVip" style="display:none;">
+                    alertantVip selected
+                </div>
             </div>
         </div>        
         <!-- ********** Fin Desplegable 2 ********** -->
         <div class="desplegable">
             <button class="collapsible"><strong>Localització</strong></button>
             <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quis corporis fugit facilis modi veritatis, ipsa sunt reiciendis in cumque neque reprehenderit maiores assumenda commodi at molestias, rerum a iure!</p>
             </div>   
         </div>      
         <!-- ********** Fin Desplegable 3 ********** -->  
         <div class="desplegable">     
             <button class="collapsible"><strong>Dades Afectat</strong></button>
             <div class="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut dolor repudiandae sed vitae laboriosam accusantium! Quae asperiores repellat consequatur optio cumque iure pariatur dolorum praesentium dignissimos, fugit, numquam corrupti sapiente?</p>
             </div>  
         </div>
         <!-- ********** Fin Desplegable 4 ********** -->    
