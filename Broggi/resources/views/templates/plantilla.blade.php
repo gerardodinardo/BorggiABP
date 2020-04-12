c5089e5b1a237d5352a759587cfe30a7d5134a86
lakaio <dancezanne@gmail.com>
2020-04-12 18:45:06 +0200

    copyright

--- Broggi/resources/views/templates/plantilla.blade.php
+++ Broggi/resources/views/templates/plantilla.blade.php
@@ -20,7 +20,7 @@
     @yield('content')
 
     <div class="card-footer text-muted">
-        <p class="text-white">Copyright 2020 CENTRE D’ESTUDIS POLITÈCNICS. Tots els drets reservats. Política de privacitat. Avís legal</p>
+        <p class="text-white">Copyright © 2020 CENTRE D’ESTUDIS POLITÈCNICS. Tots els drets reservats. Política de privacitat. Avís legal</p>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
