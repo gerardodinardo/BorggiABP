1052ffdd7e8083ba412492dcb3f435bc04517a13
lakaio <dancezanne@gmail.com>
2020-04-12 19:16:33 +0200

    container flex

--- Broggi/resources/views/crearUsuari.blade.php
+++ Broggi/resources/views/crearUsuari.blade.php
@@ -10,20 +10,20 @@
                 <h3 class="text-black text-center mt-5">Crear nou usuari/a: </h3>
                 <form class="mt-5">
                     <div class="form-group row">
-                        <label for="inputCodi" class="col-sm-2 col-form-label">Codi: </label>
-                        <div class="col-sm-10">
+                        <label for="inputCodi" class="col-sm-2 offset-sm-2 col-form-label">Codi: </label>
+                        <div class="col-sm-6">
                             <input type="text" class="form-control" id="inputCodi">
                         </div>                       
                     </div>
                     <div class="form-group row">
-                        <label for="inputUsuari" class="col-sm-2 col-form-label">Usuari/a: </label>
-                        <div class="col-sm-10">
+                        <label for="inputUsuari" class="col-sm-2 offset-sm-2 col-form-label">Usuari/a: </label>
+                        <div class="col-sm-6">
                             <input type="text" class="form-control" id="inputUsuari">
                         </div>                       
                     </div>
                     <div class="form-group row">
-                        <label for="inputContrasenya" class="col-sm-2 col-form-label">Contrasenya: </label>
-                        <div class="col-sm-10 input-group">
+                        <label for="inputContrasenya" class="col-sm-2 offset-sm-2 col-form-label">Contrasenya: </label>
+                        <div class="col-sm-6 input-group">
                             <input type="password" class="form-control" id="inputContrasenya">
                             <div class="input-group-append">
                               <div class="input-group-text" onclick="mContrasenya()"><i class="fa fa-eye"></i></div>
@@ -32,8 +32,8 @@
                     </div>
                     <fieldset class="form-group">
                         <div class="row">
-                          <legend class="col-form-label col-sm-2 pt-0">Tipus d'usuari:</legend>
-                          <div class="col-sm-10">
+                          <legend class="col-form-label col-sm-2 offset-sm-2 pt-0">Tipus d'usuari:</legend>
+                          <div class="col-sm-6">
                             <div class="form-check">
                               <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
                               <label class="form-check-label" for="gridRadios1">
@@ -50,7 +50,7 @@
                         </div>
                     </fieldset>
                     <div class="form-group row">
-                        <div class="col-10 my-4 offset-1">
+                        <div class="col-8 my-4 offset-2">
                           <button type="submit" class="btn btn-block btn-custom1">Submit</button>
                         </div>
                     </div>
