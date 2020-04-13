824407ce94dca79d31fd76158fd37ac09f77f471
Tritesio <alessiocrusella@gmail.com>
2020-04-13 19:01:18 +0200

    left

--- Broggi/resources/views/contacte.blade.php
+++ Broggi/resources/views/contacte.blade.php
@@ -8,14 +8,14 @@
         <h1 class="text-center" style="width: 100%; display: flex; position: center;">¿Qui s'ha passat la quarentena programant?</h1>
         <hr>   
         <div class="row">
-            <div class="col-sm-6 text-center">
+            <div class="col-sm-6">
                 <img src="{{ asset('img/contacte_alessio.jfif') }}" style="display: flex; position: center; margin: 0 auto; margin-bottom: 15px; ">
                 <p><strong>Correu: </strong>alessiocrusella@gmail.com</p>
                 <p><strong>Especialitat:</strong> Hacer macarrones a las 4 de la mañana.</p>
                 <p><strong>Conclusió:</strong> Si me hubiesen pagado no lo podría haber hecho mejor.</p>
                 
             </div>
-            <div class="col-sm-6 text-center">                
+            <div class="col-sm-6">                
                 <img src="{{ asset('img/contacte_dan.jfif') }}" style="display: flex; position: center;margin: 0 auto; margin-bottom: 15px; ">
                 <p><strong>Correu: </strong>dancezanne@gmail.com</p>
                 <p><strong>Especialitat:</strong> Crecí en la calle, cuidao.</p>
@@ -23,13 +23,13 @@
             </div>
         </div> 
         <div class="row mt-5">
-            <div class="col-sm-6 text-center">
+            <div class="col-sm-6">
                 <img src="{{ asset('img/contacte_alex.jfif') }}" style="display: flex; position: center;margin: 0 auto;  margin-bottom: 15px; ">
                 <p><strong>Correu: </strong>alexgarsanc@gmail.com</p>
                 <p><strong>Especialitat:</strong> Nose, que pondrías tú?</p>
                 <p><strong>Conclusió:</strong> ** Grillos **</p>
             </div>
-            <div class="col-sm-6 text-center">
+            <div class="col-sm-6">
                 <img src="{{ asset('img/contacte_jerat.jfif') }}" style="display: flex; position: center;margin: 0 auto;  margin-bottom: 15px; ">
                 <p><strong>Correu: </strong>gerardibz@gmail.com</p>
                 <p><strong>Especialitat:</strong> Shh, calla que me estoy viendo un episodio...</p>
