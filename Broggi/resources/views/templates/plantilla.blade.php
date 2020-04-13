fb38d0e9f51b7dc8fc3a9b0711e2b1c012d019e1
Tritesio <alessiocrusella@gmail.com>
2020-04-13 19:57:13 +0200

    back arrow

--- Broggi/resources/views/templates/plantilla.blade.php
+++ Broggi/resources/views/templates/plantilla.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="{{ asset('estilos.css') }}">
 </head>
 <body style="padding-top:105px;">
-    
     <nav class="navbar navbar-light fixed-top">
         <span class="navbar-brand mb-0 h1 text-light mx-auto"><a href="{{ asset('/') }}" style="text-decoration: none; color: white;"><img src="{{ asset('img/ibroggi.png') }}" alt="" style="height:60px;"> &nbsp;&nbsp;&nbsp;&nbsp;| Institut Moisés Broggi</a></span>
         <!--<img src="{{ asset('img/cruz_blanca.png')}}" height="50px" width="50px" alt="" class="m-auto">-->
@@ -20,6 +19,15 @@
 
     
     @yield('content')
+    <div id="mySidenav2" class="sidenav2">
+        <a href="#" id="back">
+            Torna a l'inici
+            <svg class="bi bi-arrow-return-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.854 5.646a.5.5 0 010 .708L3.207 9l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M13.5 2.5a.5.5 0 01.5.5v4a2.5 2.5 0 01-2.5 2.5H3a.5.5 0 010-1h8.5A1.5 1.5 0 0013 7V3a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+            </svg>
+        </a>
+      </div>
 
     <div class="card-footer text-muted mb-0">
         <p class="text-white h6">Copyright © 2020 CENTRE D’ESTUDIS POLITÈCNICS. Tots els drets reservats. Política de privacitat. Avís legal</p>
