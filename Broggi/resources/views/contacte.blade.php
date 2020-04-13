4ec8ee998b1464644b96f8b971f78092ef64aa1f
Tritesio <alessiocrusella@gmail.com>
2020-04-13 18:33:47 +0200

    About us

--- Broggi/resources/views/contacte.blade.php
+++ Broggi/resources/views/contacte.blade.php
@@ -5,24 +5,26 @@
 @section('content')<link href="{{ asset('/css/principalIncidencies.css') }}" rel="stylesheet">
   
     <div class="container border col-sm-9 mt-5 border rounded shadow-lg" style="padding: 40px;">
-        <h1>¿Qui Som?</h1>
+        <h1 class="text-center" style="width: 100%; display: flex; position: center;">¿Qui s'ha passat la quarentena programant?</h1>
         <hr>   
         <div class="row">
             <div class="col-sm-6 text-center">
-                <img src="{{ asset('img/contacte_alessio.jfif') }}" style="display: flex; position: center; margin: 0 auto;">
-                usandae consequatur voluptate praesentium quos reprehenderit distinctio possimus.
+                <img src="{{ asset('img/contacte_alessio.jfif') }}" style="display: flex; position: center; margin: 0 auto; margin-bottom: 15px; ">
+                <p><strong>Correu: </strong>alessiocrusella@gmail.com</p>
             </div>
             <div class="col-sm-6 text-center">                
-                <img src="{{ asset('img/contacte_dan.jfif') }}" style="display: flex; position: center; margin-bottom: 15px; margin: 0 auto;">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ab culpa, fugiat tempora non quo soluta, qui eius possimus et sunt nobis. Recusandae consequatur voluptate praesentium quos reprehenderit distinctio possimus.
+                <img src="{{ asset('img/contacte_dan.jfif') }}" style="display: flex; position: center;margin: 0 auto; margin-bottom: 15px; ">
+                <p><strong>Correu: </strong>gerardibz@gmail.com</p>
             </div>
         </div> 
         <div class="row mt-5">
             <div class="col-sm-6 text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ab culpa, fugiat tempora non quo soluta, qui eius possimus et sunt nobis. Recusandae consequatur voluptate praesentium quos reprehenderit distinctio possimus.
+                <img src="{{ asset('img/contacte_alex.jfif') }}" style="display: flex; position: center;margin: 0 auto;  margin-bottom: 15px; ">
+                <p><strong>Correu: </strong>alexgarsanc@gmail.com</p>
             </div>
             <div class="col-sm-6 text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ab culpa, fugiat tempora non quo soluta, qui eius possimus et sunt nobis. Recusandae consequatur voluptate praesentium quos reprehenderit distinctio possimus.
+                <img src="{{ asset('img/contacte_jerat.PNG') }}" style="display: flex; position: center;margin: 0 auto;  margin-bottom: 15px; ">
+                <p><strong>Correu: </strong>gerardibz@gmail.com</p>
             </div>
         </div> 
     </div>
