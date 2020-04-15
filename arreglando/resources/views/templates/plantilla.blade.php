f7d63317b7b7348ec8b890012a36f5e2bee4a63d
gerardodinardo <gerardibz@gmail.com>
2020-04-15 22:54:19 +0200

    ¿ahora?

--- arreglando/resources/views/templates/plantilla.blade.php
+++ arreglando/resources/views/templates/plantilla.blade.php
@@ -33,38 +33,26 @@
           </div>
         </div>
 
-        {{-- <div class="btn-group m-auto">
-            <button type="text" class="btn btn-customNAVTEXT" style="width: 200px; border-color: #e2167c; background-color: #e2167c">{{ Auth::user()->nom }}</button>
-            <button type="button" class="btn btn-customNAV dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-color: #e2167c;">
-              <span class="sr-only">Toggle Dropdown</span>
-            </button>
-            <div class="dropdown-menu mt-2">
-              <a class="dropdown-item" href="{{ route('logout') }}" style="width: 200px; border-color: #e2167c;" >Tanca la Sessió &nbsp;
-                <svg class="bi bi-power" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M5.578 4.437a5 5 0 104.922.044l.5-.866a6 6 0 11-5.908-.053l.486.875z" clip-rule="evenodd"/>
-                <path fill-rule="evenodd" d="M7.5 8V1h1v7h-1z" clip-rule="evenodd"/>
-              </svg></a>
-            </div>
-          </div> --}}
-       
+     
+         
 
           @else 
 
-         
-
           <div class="btn-group m-auto">
-            <button type="text" class="btn btn-customNAVTEXT" style="width: 200px; border-color: #e2167c; background-color: #e2167c">{{ Auth::user()->nom }}</button> --}}
+            <button type="text" class="btn btn-customNAVTEXT" style="width: 200px; border-color: #e2167c; background-color: #e2167c">Inicia Sessió!</button>
             <button type="button" class="btn btn-customNAV dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-color: #e2167c;">
               <span class="sr-only">Toggle Dropdown</span>
             </button>
             <div class="dropdown-menu mt-2">
-              <a class="dropdown-item" href="{{ route('logout') }}" style="width: 200px; border-color: #e2167c;" > &nbsp;
-                <svg class="bi bi-power" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M5.578 4.437a5 5 0 104.922.044l.5-.866a6 6 0 11-5.908-.053l.486.875z" clip-rule="evenodd"/>
-                <path fill-rule="evenodd" d="M7.5 8V1h1v7h-1z" clip-rule="evenodd"/>
-              </svg></a>
+              <a class="dropdown-item" href="#" style="width: 200px; border-color: #e2167c;" >Inicia Sessió &nbsp;
+                <svg class="bi bi-box-arrow-in-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M8.146 11.354a.5.5 0 010-.708L10.793 8 8.146 5.354a.5.5 0 11.708-.708l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0z" clip-rule="evenodd"/>
+                  <path fill-rule="evenodd" d="M1 8a.5.5 0 01.5-.5h9a.5.5 0 010 1h-9A.5.5 0 011 8z" clip-rule="evenodd"/>
+                  <path fill-rule="evenodd" d="M13.5 14.5A1.5 1.5 0 0015 13V3a1.5 1.5 0 00-1.5-1.5h-8A1.5 1.5 0 004 3v1.5a.5.5 0 001 0V3a.5.5 0 01.5-.5h8a.5.5 0 01.5.5v10a.5.5 0 01-.5.5h-8A.5.5 0 015 13v-1.5a.5.5 0 00-1 0V13a1.5 1.5 0 001.5 1.5h8z" clip-rule="evenodd"/>
+                </svg></a>
             </div>
           </div>
+
 
 
 
