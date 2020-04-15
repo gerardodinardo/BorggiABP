9f9a3c9e9ea9c995da18934319c1057db36d2f43
Tritesio <alessiocrusella@gmail.com>
2020-04-15 22:31:54 +0200

    alerts creacion user

--- arreglando/resources/views/crearUsuari.blade.php
+++ arreglando/resources/views/crearUsuari.blade.php
@@ -79,6 +79,21 @@
                 </form>
             </div>
         </div>
+        <hr>
+        {{-- usuari correcte --}}
+        <div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+          <strong>Usuari Creat Correctament!</strong>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        {{-- usuari errror --}}
+        <div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+          <strong>Oops, sembla que hi ha hagut un error!</strong>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
     </div>
 
     <script>
