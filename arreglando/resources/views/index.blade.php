9f5b074e39a8540a96c099d8ab62f244d9fb053a
Tritesio <alessiocrusella@gmail.com>
2020-04-24 08:12:10 +0200

    Botones Index Arreglados + o -

--- arreglando/resources/views/index.blade.php
+++ arreglando/resources/views/index.blade.php
@@ -10,9 +10,17 @@
           @if (Auth::check())
               {{-- AÑADIR BOTÓN PARA VOLVER AL LUGAR QUE LE CORRESPONDE. --}}
               <h3 class="text-center text-dark mt-5">Benvingut, {{ Auth::user()->nom }}!</h3>
-              <div class="text-center pb-4 mt-5">
-                <a href="{{ url('/principalIncidencies') }}" class="btn btn-custom1 mt-1">Navega a Tauler d'Incidencies</a><br/>
-                <a href="{{ route('logout') }}" class="btn btn-custom1 mt-4">- Tancar Sessió -</a>
+              <div class="row col-sm-12 text-center mt-5 d-flex justify-content-md-around">
+                <a href="{{ url('/principalIncidencies') }}" class="logged_button_redirect">
+                  Navega a Tauler d'Incidencies
+                  <svg class="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z" clip-rule="evenodd"/>
+                    <path fill-rule="evenodd" d="M2 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H2.5A.5.5 0 012 8z" clip-rule="evenodd"/>
+                  </svg>
+                </a>
+              </div>
+              <div class="row col-sm-12 text-center mt-5 d-flex justify-content-md-around">
+                <a href="{{ route('logout') }}" class="logged_button">- Tancar Sessió -</a>
               </div>
           @else
           
